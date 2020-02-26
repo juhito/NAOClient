fef71f612f7d542547cca606df066ff2c61b92c1
juhi <jussi8@hotmail.com>
2020-02-26 21:56:37 +0200

    complete remake of navigation drawer, start using fragments

--- app/src/main/java/com/example/naocontroller/MainActivity.java
+++ app/src/main/java/com/example/naocontroller/MainActivity.java
@@ -7,7 +7,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.StrictMode;
 import android.view.View;
@@ -19,6 +22,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.naocontroller.fragments.GeneralFragment;
 import com.google.android.material.navigation.NavigationView;
 
 import java.net.InetAddress;
@@ -27,103 +31,81 @@ import java.net.UnknownHostException;
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout dl;
+    private Toolbar tb;
     private ActionBarDrawerToggle t;
     private NavigationView nv;
-
-    private NAOClient client;
-    private Button b;
-    private Button message;
-    private TextView naoCommand;
-    private TextView naoMessage;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dl = findViewById(R.id.drawer_layout);
-        t = new ActionBarDrawerToggle(this, dl, R.string.drawer_open, R.string.drawer_close);
+        tb = findViewById(R.id.toolbar);
+        setSupportActionBar(tb);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        dl.addDrawerListener(t);
+        dl = findViewById(R.id.drawer_layout);
+        t = setupDrawerToggle();
+
+        t.setDrawerIndicatorEnabled(true);
         t.syncState();
 
+        dl.addDrawerListener(t);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
+        nv = findViewById(R.id.nvView);
+        setupDrawerContent(nv);
 
-        nv = findViewById(R.id.nv);
+    }
 
-        nv.setNavigationItemSelectedListener(menuItem -> {
-            int id = menuItem.getItemId();
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        return(new ActionBarDrawerToggle(this, dl, tb, R.string.drawer_open, R.string.drawer_close));
+    }
 
-            switch(id) {
-                case R.id.general:
-                    Toast.makeText(MainActivity.this, "clicked general", Toast.LENGTH_SHORT).show();
-                    break;
-                case R.id.behaviors:
-                    Toast.makeText(MainActivity.this, "clicked behaviors", Toast.LENGTH_SHORT).show();
-                    break;
-                case R.id.settings:
-                    Toast.makeText(MainActivity.this, "clicked settings", Toast.LENGTH_SHORT).show();
-                    break;
-                default:
-                    return(true);
-            }
+    private void setupDrawerContent(NavigationView view) {
+        view.setNavigationItemSelectedListener(menuItem -> {
+            selectDrawerItem(menuItem);
             return(true);
         });
+    }
 
-        b = findViewById(R.id.socketConn);
-        message = findViewById(R.id.sendMessageButton);
-        naoCommand = findViewById(R.id.naoCommand);
-        naoMessage = findViewById(R.id.naoMessage);
+    public void selectDrawerItem(MenuItem item) {
+        // Create a new fragment and specify the fragment to show based on nav item clicked.
+        Fragment fragment = null;
+        Class fragmentClass;
 
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
+        switch(item.getItemId()) {
+            case R.id.general:
+                fragmentClass = GeneralFragment.class;
+                break;
+            default:
+                fragmentClass = GeneralFragment.class;
+        }
 
         try {
-            System.out.println(ConnectNAOActivity.getIP());
-            this.client = new NAOClient(ConnectNAOActivity.getIP(), 8888);
-        } catch (Exception e) {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch(Exception e) {
             e.printStackTrace();
         }
 
-        b.setOnClickListener(v -> {
-            try {
-                NAOClient.startThreadedConnection(ConnectNAOActivity.getIP(), 8888);
-            }
-            catch(Exception e) {
-                e.printStackTrace();
-            }
-        });
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
 
-        message.setOnClickListener(v -> {
-            try {
-                String data = naoCommand.getText().toString();
-                String message = naoMessage.getText().toString();
-
-                if(message.isEmpty()) {
-                    this.client.sendMessage(data);
-                }
-                else {
-                    this.client.sendMessage(data, message);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
+        item.setChecked(true);
+        setTitle(item.getTitle());
+        dl.closeDrawers();
     }
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
+        t.syncState();
     }
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+        t.onConfigurationChanged(newConfig);
     }
 
     @Override
@@ -135,19 +117,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (t.onOptionsItemSelected(item)) {
+            return(true);
         }
-
-        if(t.onOptionsItemSelected(item)) return(true);
-
-
-        return super.onOptionsItemSelected(item);
+        return(super.onOptionsItemSelected(item));
     }
 }
---
+++ app/src/main/java/com/example/naocontroller/fragments/GeneralFragment.java
@@ -0,0 +1,19 @@
+package com.example.naocontroller.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.naocontroller.R;
+
+public class GeneralFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return(inflater.inflate(R.layout.content_main, container, false));
+    }
+}
